d2d64be4ea3cf4b26a39f81cbd0bebb338e1e7e6
Hugo H <51268820+hugoheml@users.noreply.github.com>
2023-04-05 16:09:36 +0200

    Add FetchSticketsPacks function

---
+++ constants.js
@@ -0,0 +1,6 @@
+module.exports = {
+    DISCORD_API_BASE_URL: 'https://discord.com/api/v10',
+    ENDPOINTS: {
+        STICKERS: '/sticker-packs',
+    }
+}
--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// 
+// Require dependencies
+const fetch = require('node-fetch');
+const { mkdirSync, writeFileSync } = require('fs');
+
+// Require configuration
+const { outputFolder, botToken } = require('./config.json');
+
+// Require constants
+const { DISCORD_API_BASE_URL, ENDPOINTS } = require('./constants.js');
+
+function FetchStickersPacks() {
+    return new Promise( (resolve, reject) => {
+        fetch(`${DISCORD_API_BASE_URL}${ENDPOINTS.STICKERS}`, {
+            method: 'GET',
+            headers: {
+                "Authorization": `Bot ${botToken}`
+            }
+        }).then( (res) => {
+            if (res.status !== 200) {
+
+                reject(`Discord API returned status code ${res.status}. Make sure your bot token is valid.`);
+            } else {
+
+                res.json().then( (result) => {
+                    resolve(result.stickers_packs)
+                })
+            }
+        })
+    })
+}
